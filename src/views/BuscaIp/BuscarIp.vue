710049c245ae3add0a74c98b9979aba5cbc57795
Euler Amorim <euler.amorim@hotmail.coomo>
2022-01-30 21:52:37 -0300

    Criando escopo do card de informacao da cidade

--- src/views/BuscaIp/BuscarIp.vue
+++ src/views/BuscaIp/BuscarIp.vue
@@ -18,6 +18,36 @@
               ></v-text-field>
             </div>
           </v-col>
+          <v-col cols="12">
+            <v-card class="mx-auto" max-width="980" outlined>
+              <v-row>
+                <v-col cols="3" class="border-right">
+                  <div class="text-overline mb-4">IP ADDRESS</div>
+                  <v-list-item-title class="text-h5 mb-1">
+                    192.212.174.101
+                  </v-list-item-title>
+                </v-col>
+                <v-col cols="3" class="border-right">
+                  <div class="text-overline mb-4">LOCATION</div>
+                  <v-list-item-title class="text-h5 mb-1">
+                    Brooklyn, NY 10001
+                  </v-list-item-title>
+                </v-col>
+                <v-col cols="3" class="border-right">
+                  <div class="text-overline mb-4">TIMEZONE</div>
+                  <v-list-item-title class="text-h5 mb-1">
+                    UTC-05:00
+                  </v-list-item-title>
+                </v-col>
+                <v-col cols="3">
+                  <div class="text-overline mb-4">ISP</div>
+                  <v-list-item-title class="text-h5 mb-1">
+                    SpaceX Starlink
+                  </v-list-item-title>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
         </v-row>
       </v-img>
     </v-col>
